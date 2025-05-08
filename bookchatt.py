d974932fae59c6fa76bbc42425617ecfd8e68657
CelsovSilva <celsoviniciussouzasilva@gmail.com>
2025-05-07 22:29:49 -0300

    Add files via upload

--- bookchatt.py
+++ bookchatt.py
@@ -6,6 +6,7 @@ import openpyxl
 import threading
 import tkinter as tk
 from tkinter import messagebox
+from PIL import Image, ImageTk
 
 executando = False
 PROGRESSO_ARQUIVO = "progresso.txt"
@@ -135,16 +136,29 @@ def main():
 # Interface gráfica
 janela = tk.Tk()
 janela.title("Envio de WhatsApp Automático")
-janela.geometry("320x200")
 
-titulo = tk.Label(janela, text="Envio de WhatsApp Automático", font=("Arial", 12, "bold"))
-titulo.pack(pady=10)
+# Carregar a imagem de fundo
+try:
+    imagem_pil = Image.open("teladefundoboot.jpg")
+    imagem_tk = ImageTk.PhotoImage(imagem_pil)
+    fundo_label = tk.Label(janela, image=imagem_tk)
+    fundo_label.place(x=0, y=0, relwidth=1, relheight=1)
+except FileNotFoundError:
+    messagebox.showerror("Erro", "A imagem 'bootplay.png' não foi encontrada.")
+    janela.geometry("320x200") # Define um tamanho padrão se a imagem não for carregada
 
-botao_iniciar = tk.Button(janela, text="Iniciar", width=25, command=iniciar_envio, bg="green", fg="white")
-botao_iniciar.pack(pady=5)
 
-botao_parar = tk.Button(janela, text="Parar", width=25, command=parar_envio, bg="red", fg="white")
-botao_parar.pack(pady=5)
+
+# Frame para os botões
+frame_botoes = tk.Frame(janela, bg="#ADD8E6", bd=0) # Cor de fundo e borda removida
+frame_botoes.pack(side=tk.BOTTOM, pady=20) # Botões na parte inferior
+
+# Botões
+botao_iniciar = tk.Button(frame_botoes, text="Iniciar", width=10, command=iniciar_envio, bg="green", fg="white")
+botao_iniciar.pack(side=tk.LEFT, padx=5)
+
+botao_parar = tk.Button(frame_botoes, text="Parar", width=10, command=parar_envio, bg="red", fg="white")
+botao_parar.pack(side=tk.LEFT, padx=5)
 
 def recomecar_zero():
     arquivos_excel = [arquivo for arquivo in os.listdir() if arquivo.endswith(('.xlsx', '.xls'))]
@@ -153,7 +167,7 @@ def recomecar_zero():
     else:
         messagebox.showerror("Erro", "Nenhuma planilha encontrada para reiniciar.")
 
-botao_recomecar = tk.Button(janela, text="Recomeçar do Zero", width=25, command=recomecar_zero, bg="blue", fg="white")
-botao_recomecar.pack(pady=5)
+botao_recomecar = tk.Button(frame_botoes, text="Recomeçar", width=10, command=recomecar_zero, bg="blue", fg="white")
+botao_recomecar.pack(side=tk.LEFT, padx=5)
 
 janela.mainloop()
